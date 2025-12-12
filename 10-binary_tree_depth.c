9a4fa839e9f354fd1e2a2c82912651f18b7c804a
eloyarnel <eloyaliceasanchez@gmail.com>
2025-12-12 14:19:17 -0400

    Project of Binary Tree, Task 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,7 +1,9 @@
 #include "binary_trees.h"
 /**
- * binary_tree_depth: measures the depth of a node in a binary tree
+ * binary_tree_depth - measures the depth of a node in a binary tree
  * @tree: pointer to the node to measure the depth
+ *
+ * Return: depth of the node
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
