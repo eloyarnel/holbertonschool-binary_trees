fe6885b445aeb9420b9546258c6b031f1deea529
eloyarnel <eloyaliceasanchez@gmail.com>
2025-12-12 14:17:47 -0400

    Project of Binary Tree, Task 11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,8 +1,9 @@
 #include "binary_trees.h"
 /**
- * binary_tree_size: measures the size of a binary tree
- * @tree: pointer to the root node of the tree to measure the size
- * Return: size of the tree
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: pointer to the root node of the tree
+ *
+ * Return: number of nodes in the tree
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
