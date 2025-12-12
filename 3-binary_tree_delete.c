0eac01610c5076bbf9152f8ee2efce4d77694a5c
eloyarnel <eloyaliceasanchez@gmail.com>
2025-12-12 09:38:09 -0400

    Project of Binary Tree, Task 3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 /**
- * binery_tree_delete - Deletes an entire binary tree
- * @tree: Pointer to the root node of the tree to delete
+ * binery_tree_delete: Deletes an entire binary tree
+ * @tree: Pointer to root node of the tree to delete
  * Return: void
  */
 void binary_tree_delete(binary_tree_t *tree)
