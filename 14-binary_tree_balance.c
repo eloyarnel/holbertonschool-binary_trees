6efd67633fef121f15ffe0bdd4e74b2c1af966f3
eloyarnel <eloyaliceasanchez@gmail.com>
2025-12-14 08:29:23 -0400

    Project of Binary Tree, Task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -5,10 +5,17 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-int left_height, right_height;
+int left_height = 0;
+int right_height = 0;
+
 if (tree == NULL)
 return (0);
-left_height = (int)binary_tree_balance(tree->left);
-right_height = (int)binary_tree_balance(tree->right);
+
+if (tree->left)
+left_height = (int)binary_tree_height(tree->left) + 1;
+
+if (tree->right)
+right_height = (int)binary_tree_height(tree->right) + 1;
+
 return (left_height - right_height);
 }
